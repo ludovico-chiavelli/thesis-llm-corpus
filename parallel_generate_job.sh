795695cadc598b254ef302bc55521a027935b207
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2025-03-04 13:47:56 +0000

    fix single quotes issue

--- parallel_generate_job.sh
+++ parallel_generate_job.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 declare -A configs
-configs["llama"]="--model_name='meta-llama/Llama-3.1-8B-Instruct'"
-configs["gemma"]="--model_name='google/gemma-2-9b-it'"
-configs["mistral"]="--model_name='mistralai/Mistral-7B-Instruct-v0.3'"
+configs["llama"]="--model_name=meta-llama/Llama-3.1-8B-Instruct"
+configs["gemma"]="--model_name=google/gemma-2-9b-it"
+configs["mistral"]="--model_name=mistralai/Mistral-7B-Instruct-v0.3"
 
 # (start_line, end_line) pairs
 ranges=(
