7e32b53d5098e5b2d02b1de56c8986080bb731c3
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2025-03-04 11:44:07 +0000

    implement parallelized corpus generation

---
+++ parallel_generate_job.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+declare -A configs
+configs["llama"]="--model_name='meta-llama/Llama-3.1-8B-Instruct'"
+configs["gemma"]="--model_name='google/gemma-2-9b-it'"
+configs["mistral"]="--model_name='mistralai/Mistral-7B-Instruct-v0.3'"
+
+# (start_line, end_line) pairs
+ranges=(
+    "1 1000"
+    "1001 2000"
+    "2001 3000"
+)
+
+# Call generate_job.sh for each config and range
+for key in "${!configs[@]}" 
+do
+    for range in "${ranges[@]}"
+    do
+        start_line=$(echo $range | cut -d' ' -f1)
+        end_line=$(echo $range | cut -d' ' -f2)
+        sbatch generate_job.sh corpus_generator.py ${configs[$key]} --start_line=$start_line --end_line=$end_line
+    done
+done
